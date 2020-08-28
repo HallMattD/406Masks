98fd5b71b3dfe7be757770d55af69a251d7a52c5
Matt Hall <mattd.h.11@gmail.com>
2020-08-28 17:28:14 -0600

    initial commit

---
+++ maskDonations.js
@@ -0,0 +1,25 @@
+var donationLoc = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-111.043, 45.677]
+            },
+            "properties": {
+                "popupContent": "Bozeman, MT"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-108.501, 45.783]
+            },
+            "properties": {
+                "popupContent": "Billngs, MT"
+            }
+        }
+    ]
+};
